5e37fc1f616b19320e0add089552aa7cfc0471d2
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2023-06-01 15:42:54 -0300

    class 10 - conditional rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
+import { ConditionalRenderingComponent } from './components/conditional-rendering/conditional-rendering.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
     ParentDataComponent,
-    DirectivesComponent
+    DirectivesComponent,
+    ConditionalRenderingComponent
   ],
   imports: [
     BrowserModule,
